c3e6541abbcc694b916b4d9f0b54442be2bd20bb
Tomaz Canabrava <tcanabrava@kde.org>
2017-07-24 19:44:07 +0200

    [Statistics] Helper class to hold the columns
    Signed-off-by: Tomaz Canabrava <tcanabrava@kde.org>

--- desktop-widgets/tab-widgets/TabDiveStatistics.cpp
+++ desktop-widgets/tab-widgets/TabDiveStatistics.cpp
@@ -7,12 +7,60 @@
 
 #include <QHBoxLayout>
 #include <QQuickWidget>
+#include <QQmlContext>
+
+QList<QString> ColumnStatisticsWrapper::columnNames()
+{
+    return m_columnNames;
+}
+
+QList<int> ColumnStatisticsWrapper::maxValues()
+{
+    return m_maxValues;
+}
+
+QList<int> ColumnStatisticsWrapper::meanValues()
+{
+    return m_meanValues;
+}
+
+QList<int> ColumnStatisticsWrapper::minValues()
+{
+    return m_minValues;
+}
+
+void ColumnStatisticsWrapper::setColumnNames(const QList<QString>& newColumnNames)
+{
+    m_columnNames = newColumnNames;
+    emit changed();
+}
+
+void ColumnStatisticsWrapper::setMaxValues(const QList<int>& maxValues)
+{
+    m_maxValues = maxValues;
+    emit changed();
+}
+
+void ColumnStatisticsWrapper::setMeanValues(const QList<int>& meanValues)
+{
+    m_meanValues = meanValues;
+    emit changed();
+}
+
+void ColumnStatisticsWrapper::setMinValues(const QList<int>& minValues)
+{
+    m_minValues = minValues;
+    emit changed();
+}
 
 TabDiveStatistics::TabDiveStatistics(QWidget *parent) : TabBase(parent)
 {
     auto layout = new QHBoxLayout();
     auto quickWidget = new QQuickWidget();
 
+    m_columnsDepth = new ColumnStatisticsWrapper();
+
+    quickWidget->rootContext()->setContextProperty("columnsDepthStatistics", m_columnsDepth);
     quickWidget->setSource(QUrl::fromLocalFile(":/qml/statistics.qml"));
 
     layout->addWidget(quickWidget);
--- desktop-widgets/tab-widgets/TabDiveStatistics.h
+++ desktop-widgets/tab-widgets/TabDiveStatistics.h
@@ -4,6 +4,33 @@
 
 #include "TabBase.h"
 
+/* I currently have no idea on how to deal with this.
+ */
+class ColumnStatisticsWrapper : public QObject {
+    Q_OBJECT
+public:
+    /* for a Column based statistics, QtCharts needs vectors,
+    * where each value represents a column. */
+    void setColumnNames(const QList<QString>& newColumnNames);
+    void setMinValues(const QList<int>& minValues);
+    void setMeanValues(const QList<int>& meanValues);
+    void setMaxValues(const QList<int>& maxValues);
+
+    Q_INVOKABLE QList<QString> columnNames();
+    Q_INVOKABLE QList<int> maxValues();
+    Q_INVOKABLE QList<int> meanValues();
+    Q_INVOKABLE QList<int> minValues();
+
+private:
+    QList<QString> m_columnNames;
+    QList<int> m_meanValues;
+    QList<int> m_minValues;
+    QList<int> m_maxValues;
+
+signals:
+    void changed(); // triggered when min/max/mean value changes.
+};
+
 class TabDiveStatistics : public TabBase {
 	Q_OBJECT
 public:
@@ -11,6 +38,8 @@ public:
 	~TabDiveStatistics();
 	void updateData() override;
 	void clear() override;
+private:
+    ColumnStatisticsWrapper *m_columnsDepth; // if one works, all will.
 };
 
 #endif
