70403324331ba34f2bf3d74a7e0c7d99e8eeaa57
Tomaz Canabrava <tcanabrava@kde.org>
2017-08-13 11:25:26 +0200

    [Statistics] Start the Box-and-wiskers model
    Yet another model for this...

--- desktop-widgets/tab-widgets/TabDiveStatistics.cpp
+++ desktop-widgets/tab-widgets/TabDiveStatistics.cpp
@@ -13,122 +13,71 @@
 #include <QQuickWidget>
 #include <QQmlContext>
 
-ColumnStatisticsWrapper::ColumnStatisticsWrapper(QObject* parent) : QObject(parent)
+MinAvgMaxModel::MinAvgMaxModel(QObject *parent) : QAbstractTableModel(parent)
 {
-}
-
-QStringList ColumnStatisticsWrapper::columnNames() const
-{
-    return m_columnNames;
-}
-
-QVariantList ColumnStatisticsWrapper::maxValues() const
-{
-    return m_maxValues;
-}
-
-QVariantList ColumnStatisticsWrapper::meanValues() const
-{
-    return m_meanValues;
-}
-
-QVariantList ColumnStatisticsWrapper::minValues() const
-{
-    return m_minValues;
-}
-
-void ColumnStatisticsWrapper::setColumnNames(const QStringList& newColumnNames)
-{
-    m_columnNames = newColumnNames;
-    emit columnNamesChanged(m_columnNames);
 
 }
 
-void ColumnStatisticsWrapper::setMaxValues(const QVariantList& values)
+MinAvgMaxModel::~MinAvgMaxModel()
 {
-    m_maxValues = values;
-    emit maxValuesChanged(m_maxValues);
+
 }
 
-void ColumnStatisticsWrapper::setMeanValues(const QVariantList& values)
-{
-    m_meanValues = values;
-    emit meanValuesChanged(m_meanValues);
+int MinAvgMaxModel::columnCount(const QModelIndex& parent) const {
+    return 5;
 }
 
-void ColumnStatisticsWrapper::setMinValues(const QVariantList& values)
-{
-    m_minValues = values;
-    emit minValuesChanged(m_minValues);
+int MinAvgMaxModel::rowCount(const QModelIndex& parent) const {
+    return internalData.count();
 }
 
-qreal ColumnStatisticsWrapper::max() const
+QVariant MinAvgMaxModel::data(const QModelIndex& index, int role) const
 {
-    return m_max;
+    if (role != Qt::DisplayRole) {
+        return {};
+    }
+    switch(index.column()) {
+        case 0 : Q_FALLTHROUGH();
+        case 1 : return internalData.at(index.row()).min;
+        case 2 : return internalData.at(index.row()).avg;
+        case 3 : Q_FALLTHROUGH();
+        case 4 : return internalData.at(index.row()).max;
+    }
+    return {};
 }
 
-qreal ColumnStatisticsWrapper::min() const
+QVariant MinAvgMaxModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    return m_min;
-}
-
-void ColumnStatisticsWrapper::setMax(qreal max)
-{
-    m_max = max;
-    emit maxChanged(m_max);
-}
-
-void ColumnStatisticsWrapper::setMin(qreal min)
-{
-    m_min = min;
-    emit minChanged(m_min);
+    if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
+        return rowHeaders.at(section);
+    }
+    return {};
 }
 
 
-TripDepthStatistics::TripDepthStatistics(QObject* parent) : ColumnStatisticsWrapper(parent)
+TripDepthModel::TripDepthModel(QObject* parent) : MinAvgMaxModel(parent)
 {
 }
 
-void TripDepthStatistics::repopulateData()
+void TripDepthModel::repopulateData()
 {
-    QList<QString> columnNames;
-    QVariantList minValues;
-    QVariantList meanValues;
-    QVariantList maxValues;
-    typedef std::tuple<int,int,int,QString> tripStat;
-    std::vector<tripStat> values;
+    beginResetModel();
+    internalData.clear();
+    rowHeaders.clear();
+    MinAvgMax currData;
 
     if (stats_by_trip != NULL && stats_by_trip[0].is_trip == true) {
         for (int i = 1; stats_by_trip != NULL && stats_by_trip[i].is_trip; ++i) {
             auto stats = stats_by_trip[i];
-            auto min = stats.min_depth.mm;
-            auto mean = stats.avg_depth.mm;
-            auto max = stats.max_depth.mm;
-
-            values.push_back({min, mean, max, QString(stats.location)});
+            currData.min = stats.min_depth.mm/1000;
+            currData.avg = stats.avg_depth.mm/1000;
+            currData.max = stats.max_depth.mm/1000;
+            rowHeaders.append(QString(stats.location));
+            internalData.append(currData);
         }
     }
 
-    qSort(values.begin(), values.end(), [](const tripStat& a, const tripStat& b) {
-        qDebug() << std::get<2>(a) << std::get<2>(b);
-        return std::get<2>(a) < std::get<2>(b);
-    });
-
-    for(const auto& trip : values) {
-        minValues.push_back(std::get<0>(trip)  / 1000.0);
-        meanValues.push_back(std::get<1>(trip) / 1000.0);
-        maxValues.push_back(std::get<2>(trip)  / 1000.0);
-        columnNames.push_back(std::get<3>(trip));
-    }
-    qDebug() << "Depth" << meanValues;
-    qDebug() << "Max" << maxValues;
-
-    setColumnNames(columnNames);
-    setMinValues(minValues);
-    setMeanValues(meanValues);
-    setMaxValues(maxValues);
-    setMin(0);
-    setMax(maxValues.count() ? maxValues.last().toInt() : 0);
+    endResetModel();
 }
 
 TabDiveStatistics::TabDiveStatistics(QWidget *parent) : TabBase(parent)
@@ -137,13 +86,11 @@ TabDiveStatistics::TabDiveStatistics(QWidget *parent) : TabBase(parent)
     auto quickWidget = new QQuickWidget();
 
     quickWidget->setResizeMode(QQuickWidget::SizeRootObjectToView);
-    m_columnsDepth = new TripDepthStatistics(this);
 
-    quickWidget->rootContext()->setContextProperty("columnsDepthStatistics", m_columnsDepth);
+    quickWidget->rootContext()->setContextProperty("columnsDepthStatistics", nullptr);
     quickWidget->setSource(QUrl::fromLocalFile(":/qml/statistics.qml"));
 
     layout->addWidget(quickWidget);
-    setLayout(layout);
 }
 
 TabDiveStatistics::~TabDiveStatistics()
@@ -156,5 +103,4 @@ void TabDiveStatistics::clear()
 
 void TabDiveStatistics::updateData()
 {
-    m_columnsDepth->repopulateData();
 }
--- desktop-widgets/tab-widgets/TabDiveStatistics.h
+++ desktop-widgets/tab-widgets/TabDiveStatistics.h
@@ -6,61 +6,38 @@
 #include <QString>
 #include <QList>
 #include <QVariant>
+#include <QAbstractTableModel>
+#include <QVector>
 
 /* I currently have no idea on how to deal with this.
  */
-class ColumnStatisticsWrapper : public QObject {
-    Q_OBJECT
-    Q_PROPERTY(QStringList names READ columnNames WRITE setColumnNames NOTIFY columnNamesChanged);
-    Q_PROPERTY(QVariantList minValues READ minValues WRITE setMinValues NOTIFY minValuesChanged);
-    Q_PROPERTY(QVariantList meanValues READ meanValues WRITE setMeanValues NOTIFY meanValuesChanged);
-    Q_PROPERTY(QVariantList maxValues READ maxValues WRITE setMaxValues NOTIFY maxValuesChanged);
-    Q_PROPERTY(qreal min READ min WRITE setMin NOTIFY minChanged);
-    Q_PROPERTY(qreal max READ max WRITE setMax NOTIFY maxChanged);
 
-public:
-    //TODO: Export the model to be used via QML
-    // Currently it's hardcoded.
-    ColumnStatisticsWrapper(QObject *parent = 0);
-
-    /* for a Column based statistics, QtCharts needs vectors,
-    * where each value represents a column. */
-    void setColumnNames(const QStringList& newColumnNames);
-    void setMinValues(const  QVariantList& values);
-    void setMeanValues(const QVariantList& values);
-    void setMaxValues(const  QVariantList& values);
-    void setMin(qreal min);
-    void setMax(qreal max);
-
-    QStringList columnNames() const;
-    QVariantList maxValues() const;
-    QVariantList minValues() const;
-    QVariantList meanValues() const;
-    qreal min() const;
-    qreal max() const;
-
-    virtual void repopulateData() = 0;
-private:
-    QStringList m_columnNames;
-    QVariantList m_meanValues;
-    QVariantList m_minValues;
-    QVariantList m_maxValues;
-    qreal m_min;
-    qreal m_max;
-
-signals:
-    void columnNamesChanged(const QStringList& names);
-    void minValuesChanged(const  QVariantList& minValues);
-    void meanValuesChanged(const QVariantList& meanValues);
-    void maxValuesChanged(const  QVariantList& maxValues);
-    void minChanged(qreal min);
-    void maxChanged(qreal max);
+struct MinAvgMax {
+    qreal min;
+    qreal avg;
+    qreal max;
 };
 
-class TripDepthStatistics : public ColumnStatisticsWrapper {
+class MinAvgMaxModel : public QAbstractTableModel {
     Q_OBJECT
 public:
-    TripDepthStatistics(QObject *parent = 0);
+    MinAvgMaxModel(QObject *parent = 0);
+    ~MinAvgMaxModel();
+    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    virtual void repopulateData() = 0;
+
+protected:
+    QVector<MinAvgMax> internalData;
+    QList<QString> rowHeaders;
+};
+
+class TripDepthModel : public MinAvgMaxModel {
+    Q_OBJECT
+public:
+    TripDepthModel(QObject *parent);
     void repopulateData() override;
 };
 
@@ -72,7 +49,6 @@ public:
 	void updateData() override;
 	void clear() override;
 private:
-    TripDepthStatistics *m_columnsDepth; // if one works, all will.
 };
 
 #endif
