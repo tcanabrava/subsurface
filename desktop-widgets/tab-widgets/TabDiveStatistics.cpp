c0c5a4fb7a9ba0769e6133dc69ad38fd0b25d38d
Tomaz Canabrava <tcanabrava@kde.org>
2017-08-13 12:49:35 +0200

    [Statistics] Good compromisse on names / values

--- desktop-widgets/tab-widgets/TabDiveStatistics.cpp
+++ desktop-widgets/tab-widgets/TabDiveStatistics.cpp
@@ -14,121 +14,6 @@
 #include <QQmlContext>
 #include <QVector>
 
-ColumnStatisticsWrapper::ColumnStatisticsWrapper(QObject* parent) : QObject(parent)
-{
-}
-
-QStringList ColumnStatisticsWrapper::columnNames() const
-{
-    return m_columnNames;
-}
-
-QVariantList ColumnStatisticsWrapper::maxValues() const
-{
-    return m_maxValues;
-}
-
-QVariantList ColumnStatisticsWrapper::meanValues() const
-{
-    return m_meanValues;
-}
-
-QVariantList ColumnStatisticsWrapper::minValues() const
-{
-    return m_minValues;
-}
-
-void ColumnStatisticsWrapper::setColumnNames(const QStringList& newColumnNames)
-{
-    m_columnNames = newColumnNames;
-    emit columnNamesChanged(m_columnNames);
-
-}
-
-void ColumnStatisticsWrapper::setMaxValues(const QVariantList& values)
-{
-    m_maxValues = values;
-    emit maxValuesChanged(m_maxValues);
-}
-
-void ColumnStatisticsWrapper::setMeanValues(const QVariantList& values)
-{
-    m_meanValues = values;
-    emit meanValuesChanged(m_meanValues);
-}
-
-void ColumnStatisticsWrapper::setMinValues(const QVariantList& values)
-{
-    m_minValues = values;
-    emit minValuesChanged(m_minValues);
-}
-
-qreal ColumnStatisticsWrapper::max() const
-{
-    return m_max;
-}
-
-qreal ColumnStatisticsWrapper::min() const
-{
-    return m_min;
-}
-
-void ColumnStatisticsWrapper::setMax(qreal max)
-{
-    m_max = max;
-    emit maxChanged(m_max);
-}
-
-void ColumnStatisticsWrapper::setMin(qreal min)
-{
-    m_min = min;
-    emit minChanged(m_min);
-}
-
-
-TripDepthStatistics::TripDepthStatistics(QObject* parent) : ColumnStatisticsWrapper(parent)
-{
-}
-
-void TripDepthStatistics::repopulateData()
-{
-    QList<QString> columnNames;
-    QVariantList minValues;
-    QVariantList meanValues;
-    QVariantList maxValues;
-    typedef std::tuple<int,int,int,QString> tripStat;
-    std::vector<tripStat> values;
-
-    if (stats_by_trip != NULL && stats_by_trip[0].is_trip == true) {
-        for (int i = 1; stats_by_trip != NULL && stats_by_trip[i].is_trip; ++i) {
-            auto stats = stats_by_trip[i];
-            auto min = stats.min_depth.mm;
-            auto mean = stats.avg_depth.mm;
-            auto max = stats.max_depth.mm;
-
-            values.push_back({min, mean, max, QString(stats.location)});
-        }
-    }
-
-    qSort(values.begin(), values.end(), [](const tripStat& a, const tripStat& b) {
-        return std::get<2>(a) < std::get<2>(b);
-    });
-
-    for(const auto& trip : values) {
-        minValues.push_back(std::get<0>(trip)  / 1000.0);
-        meanValues.push_back(std::get<1>(trip) / 1000.0);
-        maxValues.push_back(std::get<2>(trip)  / 1000.0);
-        columnNames.push_back(std::get<3>(trip));
-    }
-
-    setColumnNames(columnNames);
-    setMinValues(minValues);
-    setMeanValues(meanValues);
-    setMaxValues(maxValues);
-    setMin(0);
-    setMax(maxValues.count() ? maxValues.last().toInt() : 0);
-}
-
 struct MinAvgMax {
     int min;
     int avg;
@@ -165,7 +50,7 @@ public:
         setTickLength( QwtScaleDiv::MinorTick, 0 );
         setTickLength( QwtScaleDiv::MediumTick, 0 );
 
-        setLabelRotation( -60.0 );
+        setLabelRotation( -90.0 );
         setLabelAlignment( Qt::AlignLeft | Qt::AlignVCenter );
 
         setSpacing( 0 );
--- desktop-widgets/tab-widgets/TabDiveStatistics.h
+++ desktop-widgets/tab-widgets/TabDiveStatistics.h
@@ -21,56 +21,6 @@
 #include <qwt_symbol.h>
 #include <qwt_scale_draw.h>
 
-/* I currently have no idea on how to deal with this.
- */
-class ColumnStatisticsWrapper : public QObject {
-    Q_OBJECT
-    Q_PROPERTY(QStringList names READ columnNames WRITE setColumnNames NOTIFY columnNamesChanged);
-    Q_PROPERTY(QVariantList minValues READ minValues WRITE setMinValues NOTIFY minValuesChanged);
-    Q_PROPERTY(QVariantList meanValues READ meanValues WRITE setMeanValues NOTIFY meanValuesChanged);
-    Q_PROPERTY(QVariantList maxValues READ maxValues WRITE setMaxValues NOTIFY maxValuesChanged);
-    Q_PROPERTY(qreal min READ min WRITE setMin NOTIFY minChanged);
-    Q_PROPERTY(qreal max READ max WRITE setMax NOTIFY maxChanged);
-
-public:
-    //TODO: Export the model to be used via QML
-    // Currently it's hardcoded.
-    ColumnStatisticsWrapper(QObject *parent = 0);
-
-    /* for a Column based statistics, QtCharts needs vectors,
-    * where each value represents a column. */
-    void setColumnNames(const QStringList& newColumnNames);
-    void setMinValues(const  QVariantList& values);
-    void setMeanValues(const QVariantList& values);
-    void setMaxValues(const  QVariantList& values);
-    void setMin(qreal min);
-    void setMax(qreal max);
-
-    QStringList columnNames() const;
-    QVariantList maxValues() const;
-    QVariantList minValues() const;
-    QVariantList meanValues() const;
-    qreal min() const;
-    qreal max() const;
-
-    virtual void repopulateData() = 0;
-private:
-    QStringList m_columnNames;
-    QVariantList m_meanValues;
-    QVariantList m_minValues;
-    QVariantList m_maxValues;
-    qreal m_min;
-    qreal m_max;
-
-signals:
-    void columnNamesChanged(const QStringList& names);
-    void minValuesChanged(const  QVariantList& minValues);
-    void meanValuesChanged(const QVariantList& meanValues);
-    void maxValuesChanged(const  QVariantList& maxValues);
-    void minChanged(qreal min);
-    void maxChanged(qreal max);
-};
-
 class QwtPlotIntervalCurve;
 class QwtPlotCurve;
 
@@ -86,13 +36,6 @@ public:
 private:
     QwtPlotIntervalCurve *d_intervalCurve;
     QwtPlotCurve *d_curve;
-};
-
-class TripDepthStatistics : public ColumnStatisticsWrapper {
-    Q_OBJECT
-public:
-    TripDepthStatistics(QObject *parent = 0);
-    void repopulateData() override;
 };
 
 class TabDiveStatistics : public TabBase {
